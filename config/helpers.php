8169720b73a7aecbd51713aa13365ee3ef73103c
Fernando Castillo Valdés <ferncastillov@outlook.com>
2024-11-01 08:35:14 -0500

    add helpers config file

---
+++ config/helpers.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package     srv/esecorp
+ * @subpackage  config
+ * @file        helpers
+ * @author      Fernando Castillo <nando.castillo@outlook.com>
+ * @date        2024-11-01 08:34:26
+ * @version     1.0.0
+ * @description
+ */
+declare(strict_types=1);
+
+return [];
